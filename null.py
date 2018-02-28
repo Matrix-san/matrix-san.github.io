459585d15c4596bf168fae28b0a7c2e9c2dd2c02
Roman Shestakov <animeshnik2014@gmail.com>
2018-02-28 23:37:39 +0300

    py

--- null.py
+++ null.py
@@ -1,4 +1,14 @@
-# save comments only in english 
-s="lllll"
-s=s.replace("ll", "lll")
-print(s.replace("ll", "p"))
+#r = int(input())
+#p = int(input())
+#y = int(input())
+#print(int(r*((100+p)/100)*y))
+
+#x = int(input())
+#y = int(input())
+#print(x+y)
+
+#print(str("1"+'2') * int(1+2))
+
+ax+b=0
+a=int(input()) > 0
+asfyertuj5yr7ikyt8oy6!!!!!! 
