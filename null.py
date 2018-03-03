aadf70762249d4f68acf1bb1cc8510a4de3b484f
Roman Shestakov <animeshnik2014@gmail.com>
2018-03-03 23:35:36 +0300

    1

--- null.py
+++ null.py
@@ -1,14 +1,1 @@
-#r = int(input())
-#p = int(input())
-#y = int(input())
-#print(int(r*((100+p)/100)*y))
-
-#x = int(input())
-#y = int(input())
-#print(x+y)
-
-#print(str("1"+'2') * int(1+2))
-
-ax+b=0
-a=int(input()) > 0
-asfyertuj5yr7ikyt8oy6!!!!!! 
+print("Hello", "World!")
